74f886023e77c9aaed4dd8ac94547da7323f20be
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-21 23:22:38 +0800

    Played with the range and a different swapping sequence came up

--- bubbleSort.py
+++ bubbleSort.py
@@ -8,7 +8,7 @@ def bubbleSort(num):
     """using the argument for the range parameters so the number of the elements in the list can be easily changed and considering the 0 index value by '-1'
     plus since the last comparison between the last and second to the last position will satisfy the order already"""
     for i in range(len(num)-1, 0, -1): 
-    
+    # for i in range(0,len(num)): #played with this but the sequence became different
         for j in range(i):
             """this time i will be the last element index (since the step is negative); for this reason, the unsorted range will be lessening upto 0 index to 1 
             index elements unlike in Selection Sort where its last unsorted range is the last index element and its adjacent
