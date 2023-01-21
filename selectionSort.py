5f49d8ae4fea1c3d2d61c4fff3d8ad79ea499985
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-21 14:26:18 +0800

    Created a list of random numbers and defined the method for sorting; used for loop for a placeholder of unsorted index position for minimum element

---
+++ selectionSort.py
@@ -0,0 +1,17 @@
+"""Having a list with 10 random numbers"""
+
+def selectionSort (nums):
+    # for multiple iteration - nested loops
+    """only until the 9th position (which is index 8) since the last comparison between the last and second to the last position will satisfy the order 
+    alreaady and there is no need to loop for another placeholder to determine which is last"""
+    for  i in range(9): 
+        # variable will hold minimum index (position); initially, assume the first element of the unsorted part as the minimum
+        minIndex = i
+
+
+
+
+
+        
+list = [76, 13, 88, 26, 9, 51, 62, 22, 63, 31]
+print(list)
