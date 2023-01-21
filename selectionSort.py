1f65e8eaaf93721edc76a8b6e0ca7cba31bbff82
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-21 14:54:06 +0800

    Tried it in descending order and it still worked

--- selectionSort.py
+++ selectionSort.py
@@ -12,7 +12,7 @@ def selectionSort (nums):
             """select the minimum element in every iteration
             
             - to sort in descending order simply change the '<' to '>'; then the placeholder will be for the maximum element"""
-            if nums[j] < nums[minIndex]: # if the element at value of j index is less than the first element of the unsorted range (which is the minIndex)
+            if nums[j] > nums[minIndex]: # if the element at value of j index is less than the first element of the unsorted range (which is the minIndex)
                 minIndex = j # then j is the new index position of minimum element
 
         """swap the minimum element with the first element of the unsorted part"""
@@ -23,12 +23,14 @@ def selectionSort (nums):
         nums[minIndex] = temp # the old first element of the unsorted range will be swapped at the old index position of the new minimum element found
 
         # this will print out every determined minimum sequence for every index position
-        #print(nums)
+        print(nums)
         
 
 
 list = [76, 13, 88, 26, 9, 51, 62, 22, 63, 31]
 print(list)
 selectionSort(list)
-print("In ascensding order", list)
+# print("In ascending order", list)
+print("In descending order", list)
+
 
