4f06bc5951afe703142a44821c01c2b2fb476085
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-21 23:52:03 +0800

    Tried with descending order and it worked too

--- bubbleSort.py
+++ bubbleSort.py
@@ -14,7 +14,7 @@ def bubbleSort(num):
             index elements unlike in Selection Sort where its last unsorted range is the last index element and its adjacent
             
             - to sort in descending order simply change the '<' to '>' then the lesser value will be swapped towards the right"""
-            if num[j] > num[j+1]: # will compare adjacent elements (this will implement putting the biggest quantity value at the last index first upon ordering)
+            if num[j] < num[j+1]: # will compare adjacent elements (this will implement putting the biggest quantity value at the last index first upon ordering)
                 # temp = num[j]
                 # num[j] = num[j+1]
                 # num[j+1] = temp
@@ -27,19 +27,18 @@ def bubbleSort(num):
                 #print(num)
             
             # this will print out every comparison of adjacent elements (sequence)
-            print(num)
+            #print(num)
 
         # this will print out whenever all elements have been compared to its adjacent elements
-        #print(num) 
-
+        print(num) 
 
 
 list = [76, 13, 88, 26, 9, 51, 62, 22, 63, 31]
 print("Unsorted list:", list)
 
-
 bubbleSort(list)
-print("In ascending order:", end=" ") # By default Python‘s print() function ends with a newline. The end parameter in the print function is used to add any string. 
+print("In Descending Order:", end=" ")
+#print("In ascending order:", end=" ") # By default Python‘s print() function ends with a newline. The end parameter in the print function is used to add any string. 
 # At the end of the output of the print statement in python. Passing the whitespace to the end parameter (end=‘ ‘) indicates that the end character has to be 
 # identified by whitespace and not a newline.
 for i in range(len(list)):
