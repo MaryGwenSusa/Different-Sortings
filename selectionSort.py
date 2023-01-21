7ba41fc7ea10b709f6467323430f7bf775d94f10
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-21 14:32:48 +0800

    Created nested for loop for iterating the incrementing index position of the current unsorted list and used if conditon to compare each j index to the first element of the unsorted (changing) range. Finally, storing found index position of the found minimum element

--- selectionSort.py
+++ selectionSort.py
@@ -8,10 +8,17 @@ def selectionSort (nums):
         # variable will hold minimum index (position); initially, assume the first element of the unsorted part as the minimum
         minIndex = i
 
+        for j in range(i, 10): # for loop the incrementing index j in the range of i index (which is also an incrementing value) to range 10
+            """select the minimum element in every iteration
+            
+            - to sort in descending order simply change the '<' to '>'; then the placeholder will be for the maximum element"""
+            if nums[j] < nums[minIndex]: # if the element at value of j index is less than the first element of the unsorted part (which is the minIndex)
+                minIndex = j # then j is the new index position of minimum element
 
 
 
 
-        
+
+
 list = [76, 13, 88, 26, 9, 51, 62, 22, 63, 31]
 print(list)
