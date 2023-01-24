cea38991c84047c47dd316fdc2d5e4e87a4932bc
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-25 00:06:39 +0800

    Created a list of random numbers; defined a method that would looped thru the index 1 element up to the last element and stored the element of the incrementing index (starting from index 1) to a variable

---
+++ insertionSort.py
@@ -0,0 +1,8 @@
+"""Having a list with 10 random numbers"""
+
+def insertionSort(list): #for loop and while loop nested this time
+    for index in range(1,len(list)): #the variable index first value is 1 since start parameter is 1 so it would pertain to the 1st index element
+        value = list[index]
+
+assignedNum = [76, 13, 88, 26, 9, 51, 62, 22, 63, 31]
+print("Unsorted list:", assignedNum)
