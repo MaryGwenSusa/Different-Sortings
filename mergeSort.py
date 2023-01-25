689d72737a5c792b4f19653d21950b64f2caa5be
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-25 19:07:52 +0800

    Defined the main method for the Merge Sort Algorithm: first, slicing the array in half 'til there will be only one element at the end level or last leaf with the help of array slicing [:] and print functions for noting what branch/leaf/node does the elements go

--- mergeSort.py
+++ mergeSort.py
@@ -1,7 +1,30 @@
+"""Having a list with 10 random numbers
+
+Merge Sort - Divide and Conquer Algorithm
+- breaks down problem into multiple subproblems recursively until they become simple so solve; turn into a binary tree
+- solutions are combined to solve original problem
+
+*Big O - 0(n * log(n)) running time"""
+
+def mergeSort(lst):
+    if len(lst) > 1:
+        # split array in half; [:] python slices array
+        # leftList = lst[0:len(lst)//2]
+        leftList = lst[:len(lst)//2] # integer division so no remainder; goes from the first to the middle element
+        rightList = lst[len(lst)//2:] # goes from the middle to the last element
+        # rightList = lst[len(lst)//2:len(lst)]
+
+        #  this will print out every division of the list until having 1 element each
+        #print("Left leaf: ", leftList)
+        #print("Right leaf: ", rightList)ss
+
+
 # Print the array
 def printList(lst):
     for i in range(len(lst)):
-        print(lst[i], end=" ")
+        print(lst[i], end=" ") # By default Python‘s print() function ends with a newline. The end parameter in the print function is used to add any string. 
+        # At the end of the output of the print statement in python. Passing the whitespace to the end parameter (end=‘ ‘) indicates that the end character has to be 
+        # identified by whitespace and not a newline.
     print()
 
 
