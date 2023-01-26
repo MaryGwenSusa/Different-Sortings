6022cfa32a722d77c9084503772ea2b1ead50a3f
Mary Gwen Susa <Mary Gwen G. Susa>
2023-01-26 23:08:06 +0800

    Brief process of quick Sort; defined printing method and default event loop plus declared the random numbers in a list

---
+++ quickSort.py
@@ -0,0 +1,36 @@
+"""Having a list with 10 random numbers
+
+Quick Sort - Divide and Conquer Algorithm
+- breaks down problem into multiple subproblems recursively until they become simple so solve; turn into a binary tree
+- solutions are combined to solve original problem
+
+*Big O - 0(n * log(n)) running time
+*O(n^2) worst case
+
+1) Choose pivot element (usually last or random)
+2) Stores elements less than the pivot in left subarray
+3) Stores elements greater than the pivot in right subarray
+4) Call quicksort recursively on left subarray
+    Call quicksort recursively on right subarray
+
+this recursion goes on until the area size of a subarray is 1"""
+
+# Print the array
+def printList(lst):
+    for i in range(len(lst)):
+        print(lst[i], end=" ") # By default Python‘s print() function ends with a newline. The end parameter in the print function is used to add any string. 
+        # At the end of the output of the print statement in python. Passing the whitespace to the end parameter (end=‘ ‘) indicates that the end character has to be 
+        # identified by whitespace and not a newline.
+    print()
+
+
+# Driver program
+if __name__ == '__main__': #executes coroutine on the default event loop
+    assignedNum = [76, 13, 88, 26, 9, 51, 62, 22, 63, 31]
+    print("Unsorted List:", end=" ")
+    printList(assignedNum)
+
+    
+
+    print("Sorted Merged List:", end=" ")
+    printList(assignedNum)
